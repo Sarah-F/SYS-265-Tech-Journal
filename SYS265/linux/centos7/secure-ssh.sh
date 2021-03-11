486f260a94d29e24dc40f05fc10a9610d90d5f1b
Sarah-F <sfornaldes@gmail.com>
2021-03-11 18:19:06 -0500

    script + public key

--- SYS265/linux/centos7/secure-ssh.sh
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -4,3 +4,17 @@
 #adds a public key from the loccal repo or cirled from the remote repo
 #removes roots ability to ssh in
 echo "ALL YOUR CODE GOES HERE"
+
+echo -e "enter username"
+read user
+sudo useradd $user
+echo $user was created
+sudo mkdir /home/$user/.ssh
+sudo cp /home/sarah/SYS-265-Tech-Journal/SYS265/linux/public-keys/id_rsa.pub /home/$user/.ssh/authorized_keys
+sudo chmod 700 /home/$user/.ssh
+sudo chmod 600 /home/$user/.ssh/authorized_keys
+sudo chown -R $user:$user /home/$user/.ssh
+
+sudo printf '\n%s\n' 'PermitRootLogin no' >> /etc/ssh/sshd_config
+sudo systemctl restart sshd
+echo permit root login disabled
